a09043be011a7c2e3615a2f02e9ccafc89907fdd
sonchaegeon <thscorjs1002@naver.com>
2021-09-09 19:06:53 +0900

    ⚡️ :: braille response

---
+++ src/main/java/com/springboot/PINOKIO/dto/BrailleResponse.java
@@ -0,0 +1,11 @@
+package com.springboot.PINOKIO.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BrailleResponse {
+
+    private final String braille;
+}
