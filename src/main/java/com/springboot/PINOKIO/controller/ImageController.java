316eec61dbbf00e51e39b8faf5c026ce8d9e238f
sonchaegeon <thscorjs1002@naver.com>
2021-09-09 19:30:36 +0900

    ⚡️ :: ocr controller

--- src/main/java/com/springboot/PINOKIO/controller/ImageController.java
+++ src/main/java/com/springboot/PINOKIO/controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.springboot.PINOKIO.controller;
 
 import com.springboot.PINOKIO.dto.BrailleResponse;
+import com.springboot.PINOKIO.dto.OcrResponse;
 import com.springboot.PINOKIO.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,7 +18,12 @@ public class ImageController {
     private final ImageService imageService;
 
     @GetMapping("braille")
-    public ResponseEntity<BrailleResponse> BrailleUpload(@ModelAttribute("file") MultipartFile file) {
+    public ResponseEntity<BrailleResponse> brailleUpload(@ModelAttribute("file") MultipartFile file) {
         return new ResponseEntity<>(imageService.brailleUpload(file), HttpStatus.OK);
+    }
+
+    @GetMapping("ocr")
+    public ResponseEntity<OcrResponse> ocrUpload(@ModelAttribute("file") MultipartFile file) {
+        return new ResponseEntity<>(imageService.ocrUpload(file), HttpStatus.OK);
     }
 }
