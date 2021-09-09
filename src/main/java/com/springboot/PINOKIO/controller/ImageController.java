02363628dff64d152fe758f296fea1b4e7a2664d
sonchaegeon <thscorjs1002@naver.com>
2021-09-09 19:06:46 +0900

    ⚡️ :: braille controller

--- src/main/java/com/springboot/PINOKIO/controller/ImageController.java
+++ src/main/java/com/springboot/PINOKIO/controller/ImageController.java
@@ -1,15 +1,23 @@
 package com.springboot.PINOKIO.controller;
 
+import com.springboot.PINOKIO.dto.BrailleResponse;
+import com.springboot.PINOKIO.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/image")
+@RequestMapping("image")
+@RequiredArgsConstructor
 public class ImageController {
 
-    @GetMapping
-    public String ImageUpload(@ModelAttribute("file") MultipartFile file) {
-        //AI 호출
+    private final ImageService imageService;
+
+    @GetMapping("braille")
+    public ResponseEntity<BrailleResponse> BrailleUpload(@ModelAttribute("file") MultipartFile file) {
+        return new ResponseEntity<>(imageService.brailleUpload(file), HttpStatus.OK);
     }
 }
